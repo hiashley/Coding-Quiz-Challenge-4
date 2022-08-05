964b76e625771eb4b324d1014cc895a2adbbb0fd
ashley31836 <hiashleyyu@gmail.com>
2022-08-05 02:56:42 -0700

    html and js changes

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+var questions = [
+  {
+    q: 'The content of the page (such as your pictures, text, links) will show up here.',
+    choices: ['Head', 'Body', 'Style', 'Folder'],
+    a: 'Body'
+  },
+  {
+    q: 'JavaScript files have the file extension (the bit after the name):',
+    choices: ['.js', '.css', '.html', '.java'],
+    a: '.js'
+  },
+  {
+    q: 'Defines a division or a section in an HTML document. Used to group block-elements to format them with CSS',
+    choices: ['<caption>', '<span>', '<div>', '<group>'],
+    a: '<div>'
+  },
+  {
+    q: 'To make a comment in HTML you use ______.',
+    choices: ['//', '<!-- -->', '/*', '#'],
+    a: '<!-- -->' 
+  },
+  {
+    q: 'What is a variable?',
+    choices: ['Store values so we can use them once.', 'Store values so we can use them but cannot change them.', "Store values in containers so we can't use them later.", 'Store values so we can use them later and change them from the code.'],
+    a: 'Store values so we can use them later and change them from the code.'
+  }
+];
+
+var startQuiz = function() {
+  
+  
+}
